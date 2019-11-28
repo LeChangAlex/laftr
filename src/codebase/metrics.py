690b9c5e76384c9f0edd015e8753b564a671cd0e
Andrew Chong Li <acli@edu.uwaterloo.ca>
2019-11-28 18:48:29 -0500

    WIP

--- src/codebase/metrics.py
+++ src/codebase/metrics.py
@@ -117,11 +117,10 @@ def DeltaErr(Y, Ypred, A):
     return abs(avg_err_1 - avg_err_0)
 
 def LogRegressionCoeff(Ypred, A):
-    m = LogisticRegression().fit(A, Ypred.astype(int))
-
     eq = len(Ypred[Ypred == 0])
     if eq == 0 or eq == len(Ypred):
         return 0
+    m = LogisticRegression().fit(A, Ypred.astype(int))
     print(m.get_params())
 
 def NLL(Y, Ypred, eps=eps):
